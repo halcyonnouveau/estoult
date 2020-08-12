9c468e7ad9a61626d9a50112b55c3fb5cc58854d
Justin Duch <justin@justinduch.com>
2020-08-13 00:10:39 +1000

    add ability for subqueries

--- README.md
+++ README.md
@@ -10,59 +10,70 @@ It is **NOT** an ORM.
 from estoult import Database, Query, Field
 
 db = Database(
-    user='user',
-    password='pass',
-    port='port',
-    database='database'
+    user="user",
+    password="pass",
+    port="port",
+    database="database"
 )
 
 
 class BaseSchema(db.Schema):
-    archive = Field(int, 'archive', default=0)
-    created = Field(str, 'created', default='0000-00-00')
-    updated = Field(str, 'updated', default='0000-00-00')
+    id = Field(int, "id")
+    archive = Field(int, "archive", default=0)
+    created = Field(str, "created", default="0000-00-00")
+    updated = Field(str, "updated", default="0000-00-00")
 
 
 class User(BaseSchema):
     table_name = "Users"
+    person_id = Field(int, "person_id", null=False)
+    password = Field(str, "password", null=False)
 
-    id = Field(int, 'id')
-    person_id = Field(int, 'person_id')
-    password = Field(str, 'password')
+    @classmethod
+    def get_by_id(cls, id)
+        # Get single user
+        return (Query(cls)
+            .get()
+            .where(**{str(cls.id): 1})
+            .execute())
 
 
 class Person(BaseSchema):
     table_name = "Persons"
 
-    id = Field(int, 'id')
-    email = Field(str, 'email')
-    first_name = Field(str, 'first_name')
-    last_name = Field(str, 'last_name')
+    email = Field(str, "email")
+    first_name = Field(str, "first_name")
+    last_name = Field(str, "last_name")
 
     @classmethod
     def validate(cls, row):
         pk, changeset = super().validate(row)
+
         # Do some additional validation here
+        if changeset["email"].endswith(".fr")
+            raise Exception("The French are banned.")
+
         return pk, changeset
 
 
 # Select all users
-(Query(User)
+users = (
+    Query(User)
     .select()
-    .execute())
+    .execute()
+)
 
-# Left join with Person
-(Query(User, Person)
+# Join users with persons
+users_join = (
+    Query(User, Person)
     .select(User.id, Person.first_name, Person.last_name)
     .left_join(Person, on=[Person.id, User.person_id])
     .where(**{str(Person.archive): 0})
-    .execute())
+    .execute()
+)
 
-# Get single user
-(Query(User)
-    .get()
-    .where(**{str(User.id): 1})
-    .execute())
+# Use classmethod we made
+user = User.get_by_id(1)
 
 # Create a person
 person_id = Person.insert({
--- estoult.py
+++ estoult.py
@@ -1,3 +1,5 @@
+import re
+
 try:
     import sqlite3
 except ImportError:
@@ -18,15 +20,18 @@ class EstoultError(Exception):
     pass
 
 
-class Field:
+def _strip(string):
+    return string.rstrip(" ").rstrip(",")
 
+
+class Field:
     def __init__(self, type, name, **kwargs):
         self.type = type
         self.name = name
 
-        self.primary_key = kwargs.get('primary_key') is True
-        self.null = kwargs.get('null')
-        self.default = kwargs.get('default')
+        self.primary_key = kwargs.get("primary_key") is True
+        self.null = kwargs.get("null")
+        self.default = kwargs.get("default")
 
     @property
     def full_name(self):
@@ -67,7 +72,7 @@ class Schema:
             if isinstance(f, Field):
                 fields[key] = f
 
-                if (f.primary_key is True or (key == 'id' and pk is None)):
+                if f.primary_key is True or (key == "id" and pk is None):
                     pk = key
 
         return pk, fields
@@ -109,9 +114,7 @@ class Schema:
             params.append(value)
             sql += f"{key} = %s, "
 
-        sql = sql[:-2]
-
-        return cls._database_.insert(sql, params)
+        return cls._database_.insert(_strip(sql), params)
 
     @classmethod
     def update(cls, old, new):
@@ -123,7 +126,7 @@ class Schema:
             params.append(value)
             sql += f"{key} = %s, "
 
-        sql = f"{sql[:-2]} where {pk} = {changeset[pk]}"
+        sql = f"{_strip(sql)} where {pk} = {changeset[pk]}"
 
         return cls._database_.sql(sql, params)
 
@@ -131,39 +134,40 @@ class Schema:
 class Query:
     def __init__(self, *schemas):
         self.schemas = schemas
-
-        for s in schemas:
-            s()
+        [s() for s in schemas]
 
         self.method = None
 
         self._query = ""
         self._params = []
 
-    def select(self, *args, method="select"):
-        self.method = method
+    @staticmethod
+    def sanitise_query(query):
+        return re.sub(r"^[a-zA-Z_][a-zA-Z0-9_\$]*$", "", query)
+
+    def select(self, *args):
+        self.method = "select"
 
         if len(args) < 1:
             args = "*"
         else:
-            args = ", ".join([
-                a.full_name if isinstance(a, Field) else a for a in args
-            ])
+            args = ", ".join([str(a) for a in args])
 
         self._query = f"select {args} from {self.schemas[0].table_name}\n"
 
         return self
 
     def get(self, *args):
-        self.select(*args, method="get")
+        self.select(*args)
+        self.method = "get"
         return self
 
-    def left_join(self, j_schema, on):
-        if j_schema not in self.schemas:
+    def left_join(self, schema, on):
+        if schema not in self.schemas:
             raise EstoultError("Schema not added to Query")
 
-        q = f"{on[0].full_name} = {on[1].full_name}"
-        self._query += f"left join {j_schema.table_name} on {q}\n"
+        q = f"{str(on[0])} = {str(on[1])}"
+        self._query += f"left join {schema.table_name} on {q}\n"
 
         return self
 
@@ -178,13 +182,17 @@ class Query:
             self._query += f"{key} = '%s', "
             self._params.append(value)
 
-        self._query = f"{self._query[:-2]}\n"
+        self._query = f"{_strip(self._query)}\n"
 
         return self
 
     def execute(self):
         func = getattr(self.schemas[0], self.method)
         return func(self._query, self._params)
+
+    def __str__(self):
+        query = self._query % self._params
+        return f"({Query.sanitise_query(query)})"
 
 
 class Database:
@@ -205,17 +213,14 @@ class Database:
         conn = self.connect()
 
         try:
-            cur = conn.cursor()
-            cur.execute(query, params)
-
-            cols = [col[0] for col in cur.description]
-            rows = [dict(zip(cols, row)) for row in cur.fetchall()]
-
-            return rows
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            cols = [col[0] for col in cursor.description]
+            return [dict(zip(cols, row)) for row in cursor.fetchall()]
         except (Exception, mysql.Error) as err:
             raise err
         finally:
-            cur.close()
+            cursor.close()
             conn.close()
 
     def get(self, query, params):
@@ -250,3 +255,9 @@ class Database:
             raise err
         finally:
             cursor.close()
+
+
+class fn:
+    @staticmethod
+    def count(field):
+        return f"count({str(field)})"
